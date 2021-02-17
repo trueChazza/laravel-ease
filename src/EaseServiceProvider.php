0b16b3841dacc48577c7feffa6d97eafde284ad6
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-02-17 14:17:12 +1300

    feat(core): add EaseServiceProvider

---
+++ src/EaseServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Cgnetwork\Ease;
+
+use Illuminate\Support\ServiceProvider;
+
+class EaseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
