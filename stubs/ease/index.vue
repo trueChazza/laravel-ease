c47febe4891a0d207ab91873664f1378ee5d384a
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-02-20 23:59:03 +1300

    refactor(common): refactor stub index.vue

--- stubs/ease/index.vue
+++ stubs/ease/index.vue
@@ -1,9 +1,43 @@
 <template>
-    <h1>Welcome to Ease</h1>
+    <div class="index">
+
+        <img alt="Vue logo" src="https://raw.githubusercontent.com/vitejs/vite/main/packages/create-app/template-vue/src/assets/logo.png" />
+
+        <h1>{{ state.message }}</h1>
+
+        <p>
+            <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+            <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+        </p>
+
+        <button @click="state.count++">count is: {{ state.count }}</button>
+        <p>
+            Edit
+            <code>resources/js/pages/ease/index.vue</code> to test hot module replacement.
+        </p>
+    </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { reactive } from 'vue'
 
-}
+const state = reactive({
+    message: 'Hello Vue 3 + Vite',
+    count: 0
+})
 </script>
+
+<style scoped>
+.index {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
