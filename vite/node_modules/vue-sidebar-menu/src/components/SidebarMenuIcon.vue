5cce4cbbf6faf47524fc053230fba98255a817af
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-02-22 16:44:15 +1300

    build(core): add vue-sidebar-menu

---
+++ vite/node_modules/vue-sidebar-menu/src/components/SidebarMenuIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <component
+    :is="icon.element ? icon.element : 'i'"
+    class="vsm--icon"
+    :class="typeof icon === 'string' || (icon instanceof String) ? icon : icon.class"
+    aria-hidden="true"
+    v-bind="icon.attributes"
+  >
+    {{ icon.text }}
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'SidebarMenuIcon',
+  props: {
+    icon: {
+      type: [String, Object],
+      default: ''
+    }
+  }
+}
+</script>
