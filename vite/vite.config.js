c09c72300278b067b2262a4b0951c7f8e5c1062b
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-04-04 21:12:47 +1200

    refactor(core): refactor directory to components from laravel

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
       vue(),
       voie({
-          pagesDir: '../../../../resources/js/laravel'
+          pagesDir: '../../../../resources/js/components'
       })
   ]
 })
