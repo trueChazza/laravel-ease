dcfbfa8a696ecdb18eaf8de0a11f90020ef8dd07
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-04-04 21:11:15 +1200

    build(core): add tailwindcss

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'voie-pages'
-
 import App from './App.vue'
+import './index.css'
 
 const router = createRouter({
     history: createWebHistory(),
