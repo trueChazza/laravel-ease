465e9dbc778b9a10fa7751e7c391e8f0b53c7b0d
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-04-04 21:10:44 +1200

    build(core): remove vue-sidebar-menu

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,30 +1,9 @@
 <template>
-    <sidebar-menu :menu="menu" />
-
     <router-view />
 </template>
 
 <script>
-import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
-import { SidebarMenu } from 'vue-sidebar-menu'
-
 export default {
-    components: {
-        SidebarMenu
-    },
-    computed: {
-        menu() {
-            return [
-                {
-                    header: 'Navigation',
-                    hiddenOnCollapse: true
-                },
-                ...this.$router.getRoutes().map(route => ({
-                    href: route.path,
-                    title: route.name
-                }))
-            ]
-        }
-    }
+
 }
 </script>
