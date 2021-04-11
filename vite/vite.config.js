e1f224f788a1695ea7846426412078906f544aa4
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-04-12 06:10:54 +1200

    fix(core): add server watch use polling
    Closes #3

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -3,10 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import voie from 'vite-plugin-voie'
 
 export default defineConfig({
-  plugins: [
-      vue(),
-      voie({
-          pagesDir: '../../../../resources/js/components'
-      })
-  ]
+    plugins: [
+        vue(),
+        voie({
+            pagesDir: '../../../../resources/js/components'
+        })
+    ],
+    server: {
+        watch: {
+            usePolling: true
+        }
+    }
 })
