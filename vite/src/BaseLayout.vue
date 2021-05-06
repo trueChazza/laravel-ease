0aa15e66a24fc45f89e0b0536d5ddc2785d0031b
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-05-06 17:30:21 +1200

    feat(common): adds mobile responsive layout
    Closes #7

--- vite/src/BaseLayout.vue
+++ vite/src/BaseLayout.vue
@@ -1,72 +1,50 @@
 <template>
     <div class="h-screen flex overflow-hidden bg-white">
-        <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-        <div class="fixed inset-0 flex z-40 md:hidden" role="dialog" aria-modal="true">
-            <!--
-              Off-canvas menu overlay, show/hide based on off-canvas menu state.
 
-              Entering: "transition-opacity ease-linear duration-300"
-                From: "opacity-0"
-                To: "opacity-100"
-              Leaving: "transition-opacity ease-linear duration-300"
-                From: "opacity-100"
-                To: "opacity-0"
-            -->
-            <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+        <TransitionRoot as="template" :show="sidebarOpen">
 
-            <!--
-              Off-canvas menu, show/hide based on off-canvas menu state.
+            <Dialog as="div" static class="fixed inset-0 flex z-40 md:hidden" @close="sidebarOpen = false" :open="sidebarOpen">
+                <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
+                    <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+                </TransitionChild>
 
-              Entering: "transition ease-in-out duration-300 transform"
-                From: "-translate-x-full"
-                To: "translate-x-0"
-              Leaving: "transition ease-in-out duration-300 transform"
-                From: "translate-x-0"
-                To: "-translate-x-full"
-            -->
-            <div class="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-                <!--
-                  Close button, show/hide based on off-canvas menu state.
+                <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
+                    <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
 
-                  Entering: "ease-in-out duration-300"
-                    From: "opacity-0"
-                    To: "opacity-100"
-                  Leaving: "ease-in-out duration-300"
-                    From: "opacity-100"
-                    To: "opacity-0"
-                -->
-                <div class="absolute top-0 right-0 -mr-12 pt-2">
-                    <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span class="sr-only">Close sidebar</span>
-                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
+                        <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+                            <div class="absolute top-0 right-0 -mr-12 pt-2">
+                                <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
+                                    <span class="sr-only">Close sidebar</span>
+                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </TransitionChild>
 
-                <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+                        <div class="flex-shrink-0 flex items-center px-4 space-x-4">
+                            <img class="h-8 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" alt="logo">
+                            <h1>Ease</h1>
+                        </div>
 
-                    <div class="flex-shrink-0 flex items-center px-4 space-x-3">
-                        <img class="h-8 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" alt="logo">
-                        <h1>Ease</h1>
+                        <div class="mt-5 flex-1 h-0 overflow-y-auto">
+                            <nav class="mt-5 px-2 space-y-1">
+                                <template v-for="item in getMenu">
+                                    <router-link :to="item.path" active-class="bg-gray-100 text-gray-900" class="group flex items-center px-2 py-2 text-base font-medium rounded-md hover:text-gray-900 hover:bg-gray-100">
+                                        {{ item.name }}
+                                    </router-link>
+                                </template>
+                            </nav>
+                        </div>
                     </div>
+                </TransitionChild>
 
-                    <nav class="mt-5 px-2 space-y-1">
-                        <template v-for="item in getMenu">
-                            <router-link :to="item.path" active-class="bg-gray-100 text-gray-900" class="group flex items-center px-2 py-2 text-base font-medium rounded-md hover:text-gray-900 hover:bg-gray-100">
-                                {{ item.name }}
-                            </router-link>
-                        </template>
-                    </nav>
+                <div class="flex-shrink-0 w-14" aria-hidden="true">
+                    <!-- Dummy element to force sidebar to shrink to fit close icon -->
                 </div>
-            </div>
+            </Dialog>
+        </TransitionRoot>
 
-            <div class="flex-shrink-0 w-14">
-                <!-- Force sidebar to shrink to fit close icon -->
-            </div>
-        </div>
-
-        <!-- Static sidebar for desktop -->
         <div class="hidden md:flex md:flex-shrink-0">
             <div class="flex flex-col w-64">
 
@@ -98,7 +76,8 @@
         <div class="flex flex-col w-0 flex-1 overflow-hidden">
 
             <div class="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
-                <button class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+
+                <button @click="sidebarOpen = true" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span class="sr-only">Open sidebar</span>
                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -122,7 +101,16 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+import { Dialog, DialogOverlay, TransitionChild, TransitionRoot } from '@headlessui/vue'
+
 export default {
+    components: {
+        Dialog,
+        DialogOverlay,
+        TransitionChild,
+        TransitionRoot
+    },
     computed: {
         getMenu() {
             return this.$router.getRoutes().map(route => ({
@@ -132,6 +120,13 @@ export default {
         },
         getRouteName() {
             return this.$route.name
+        }
+    },
+    setup() {
+        const sidebarOpen = ref(false)
+
+        return {
+            sidebarOpen
         }
     }
 }
